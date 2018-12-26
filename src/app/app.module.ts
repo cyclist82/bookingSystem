6193151659c3593e2647729dffa6fdaa0d879105
Leif Lampater <leiflampater@gmail.com>
2018-12-26 21:11:13 +0100

    ADD proxy config, ADD rental details style improvements, ADD pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './common/header/header.component';
+import {AppRoutingModule} from './app-routing.module';
 import {RentalModule} from './rental/rental.module';
-import {RentalRoutingModule} from './rental/shared/rental-routing.module';
+import {UppercasePipe} from './common/pipes/upperc.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RentalModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RentalModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -5,21 +5,27 @@ import {RentalComponent} from './rental.component';
 import {CommonModule} from '@angular/common';
 import {RentalService} from './shared/rental.service';
 import {RentalRoutingModule} from './shared/rental-routing.module';
-import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import {RentalDetailComponent} from './rental-detail/rental-detail.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgPipesModule} from 'ngx-pipes';
+import {UppercPipe} from '../common/pipes/upperc.pipe';
 
 @NgModule({
-  declarations: [
-    RentalListComponent,
-    RentalListItemComponent,
-    RentalComponent,
-    RentalDetailComponent],
-  imports: [
-    CommonModule,
-    RentalRoutingModule
-  ],
-  providers: [
-    RentalService
-  ]
+    declarations: [
+        RentalListComponent,
+        RentalListItemComponent,
+        RentalComponent,
+        RentalDetailComponent,
+        UppercPipe],
+    imports: [
+        CommonModule,
+        RentalRoutingModule,
+        HttpClientModule,
+        NgPipesModule,
+    ],
+    providers: [
+        RentalService
+    ]
 })
 export class RentalModule {
 
