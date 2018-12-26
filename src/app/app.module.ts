9c0370aea1e977c479e1916e5bcdcbf0635a2f8b
Leif Lampater <leiflampater@gmail.com>
2018-12-26 21:14:41 +0100

    MINOR FIX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './common/header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {RentalModule} from './rental/rental.module';
-import {UppercasePipe} from './common/pipes/upperc.pipe';
 
 @NgModule({
     declarations: [
