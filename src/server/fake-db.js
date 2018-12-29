0f0dec5b293ae25bb2fc6fd7d930cbc6462c7080
Leif Lampater <leiflampater@gmail.com>
2018-12-29 13:19:30 +0100

    ADD register, ADD login and auth moddleware routes on server

--- src/server/fake-db.js
+++ src/server/fake-db.js
@@ -1,4 +1,5 @@
 const Rental = require('./models/rental');
+const User = require('./models/user');
 
 class FakeDB {
     constructor() {
@@ -34,22 +35,32 @@ class FakeDB {
                 shared: true,
                 description: "Very nice apartment in center of the city.",
                 dailyRate: 23
-            }]
+            }];
+        this.users = [{
+            username: "testuser",
+            email: "test@gmail.com",
+            password: "testtest"
+        }]
     }
 
     async cleanDb() {
+        await User.deleteMany({});
         await Rental.deleteMany({});
     }
 
     pushRentalsToDb() {
+        const user = new User(this.users[0]);
         this.rentals.forEach((rental) => {
             const newRental = new Rental(rental);
+            newRental.user = user;
+            user.rentals.push(newRental);
             newRental.save();
-        })
+        });
+        user.save();
     }
 
-    seedDb() {
-        this.cleanDb();
+    async seedDb() {
+        await this.cleanDb();
         this.pushRentalsToDb();
     }
 
---
+++ src/server/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        min: [4, 'Zu kurz, bitte mindestens 4 Zeichen'],
+        max: [32, 'Zu lang, bitte maximal 32 Zeichen']
+    },
+    email: {
+        type: String,
+        min: [4, 'Zu kurz, bitte mindestens 4 Zeichen'],
+        max: [32, 'Zu lang, bitte maximal 32 Zeichen'],
+        unique: true,
+        lowercase: true,
+        required: 'EMail wird benötigt',
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    password: {
+        type: String,
+        min: [8, 'Zu kurz, bitte mindestens 8 Zeichen'],
+        max: [32, 'Zu lang, bitte maximal 32 Zeichen'],
+        required: 'Passwort wird benötigt'
+    },
+    rentals: [{type: Schema.Types.ObjectId, ref: 'Rental'}]
+});
+
+userSchema.methods.hasSamePassword = function (requestedPassword) {
+    return bcrypt.compareSync(requestedPassword, this.password);
+}
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
